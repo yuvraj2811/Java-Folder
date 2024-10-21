ce631bb32a505225fbd86c370194fd4da224616a
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2024-10-21 15:24:04 +0530

    Q3 final keyword

---
+++ Java 3 Practice Set/src/Building.java
@@ -0,0 +1,14 @@
+public final class Building {
+
+}
+// class House extends Building{
+//
+// }
+
+ class Result{
+     public static void main(String[] args) {
+         System.out.println("Java can not inherit the final class it show this error");
+         System.out.println();
+         System.out.println("java: cannot inherit from final Building");
+     }
+ }
