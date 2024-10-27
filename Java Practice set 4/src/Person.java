deb361d0ae65d350b35742c7f02e8048dd461cf9
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2024-10-28 01:51:14 +0530

    Q4 java 4th practice set

---
+++ Java Practice set 4/src/Person.java
@@ -0,0 +1,81 @@
+
+
+public class Person {
+
+    private String name;
+  private  int age;
+
+  public Person(){}
+
+ public Person(String name,int age){
+      this.name=name;
+      this.age=age;
+  }
+  public String getName(){
+     return name ;
+  }
+  public int getAge(){
+     return age;
+  }
+
+}
+class Employee1 extends Person{
+    private double salary;
+    private String department;
+
+   public Employee1(String name , int age ,double salary,String department){
+        super(name,age);
+        this.salary=salary;
+        this.department=department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+        public void emloyeeDetails(){
+            System.out.println("Name of employee - "+getName());
+            System.out.println("Age - "+getAge());
+            System.out.println("Salary - "+getSalary());
+            System.out.println("Department - "+getDepartment());
+
+        }
+}
+
+class Manager1 extends Employee1{
+    private double bonous;
+
+    Manager1(String name ,int age ,double salary,String department,double bonous){
+        super(name,age,salary,department);
+        this.bonous=bonous;
+    }
+
+    @Override
+    public double getSalary() {
+        double salaryAndBonous= super.getSalary()+bonous;
+        return salaryAndBonous;
+    }
+    public void managerDetails1(){
+        System.out.println("Name of employee - "+getName());
+        System.out.println("Age - "+getAge());
+        System.out.println("Salary - "+getSalary());
+        System.out.println("Department - "+getDepartment());
+
+    }
+
+}
+class Main3{
+    public static void main(String[] args) {
+
+        Employee1 employee1 = new Employee1("Gaurav",25,50000,"FullStack Developer");
+        employee1.emloyeeDetails();
+        System.out.println("*------------------------------------------------------------------------------");
+        Manager1 manager3 =new Manager1("Abhishek",25,50000,"Sales Department",3000);
+        manager3.managerDetails1();
+        
+    }
+}
